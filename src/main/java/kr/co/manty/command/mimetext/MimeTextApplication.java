e33555ac5fc41415442cd700f8bd1d47e7b5f126
Manty <jibum.jung@nhn.com>
2021-03-16 16:06:59 +0900

    add version option

--- src/main/java/kr/co/manty/command/mimetext/MimeTextApplication.java
+++ src/main/java/kr/co/manty/command/mimetext/MimeTextApplication.java
@@ -6,12 +6,15 @@ import javax.mail.internet.MimeUtility;
 import java.io.UnsupportedEncodingException;
 
 public class MimeTextApplication {
+    private static final String VERSION = "mimetext 1.0 (author Manty)";
     public static void main(String[] args) {
         Option help = new Option("h", "help", false , "print help.");
-        Option decode = new Option("d", "decode", true, "decode mimetext");
-        Option encode = new Option("e", "encode", true, "encode mimetext");
+        Option version = new Option("v", "version", false, "print version.");
+        Option decode = new Option("d", "decode", true, "decode mimetext.");
+        Option encode = new Option("e", "encode", true, "encode mimetext.");
         Options options = new Options();
         options.addOption(help);
+        options.addOption(version);
         options.addOption(decode);
         options.addOption(encode);
 
@@ -31,12 +34,22 @@ public class MimeTextApplication {
                 HelpFormatter helpFormatter = new HelpFormatter();
                 helpFormatter.printHelp("mimetext -e '가나다라'", options);
             }
+
+            if ( commandLine.hasOption("version")) {
+                output = VERSION;
+            }
+
             if ( commandLine.hasOption("decode")) {
                 output = MimeUtility.decodeText(commandLine.getOptionValue("decode"));
             }
 
             if ( commandLine.hasOption("encode")) {
                 output = MimeUtility.encodeText(commandLine.getOptionValue("encode"), "utf-8", "B");
+            }
+
+            if (output == null) {
+                HelpFormatter helpFormatter = new HelpFormatter();
+                helpFormatter.printHelp("mimetext -e '가나다라'", options);
             }
         } catch (UnsupportedEncodingException e) {
             System.out.println(e.getMessage());
